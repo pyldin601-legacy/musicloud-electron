1336a077b0297668e0652d063d13ad8536ebb284
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2019-02-20 11:07:51 +0200

    Add ffprobe-static

--- tests/ffmpeg-static.spec.js
+++ tests/ffmpeg-static.spec.js
@@ -1,7 +1,13 @@
 const ffmpeg = require('ffmpeg-static');
+const ffprobe = require('ffprobe-static');
 const fs = require('fs');
 
 test('Should return the path of a statically linked ffmpeg binary on the local filesystem', () => {
   const path = ffmpeg.path;
+  expect(fs.existsSync(path)).toBeTruthy();
+});
+
+test('Should return the path of a statically linked ffprobe binary on the local filesystem', () => {
+  const path = ffprobe.path;
   expect(fs.existsSync(path)).toBeTruthy();
 });
